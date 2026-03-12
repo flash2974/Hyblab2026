21021a2d110c57dad4d212d1ebe74da6c3d62f06
Nathan <nathan.domenichini@gmail.com>
2026-03-12 09:47:18 +0100

    fix

---
+++ lanouvellerepublique/server.cjs
@@ -0,0 +1,17 @@
+'use strict';
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve the Vite build output (dist/ folder)
+app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, '../__common-logos__')));
+
+// SPA fallback: serve index.html for all non-file routes (Vue Router history mode)
+app.use(function (req, res) {
+	res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+module.exports = app;
